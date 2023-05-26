4915057d2eb678ad2069d5cbbaca4ce73bad7bb7
Amine Med REMMACHE <amine.med.remmache@gmail.com>
2023-05-27 00:33:03 +0100

    integrated jenkins
    inhanced organisation

--- provisions/provision-appEnv.sh
+++ provisions/provision-appEnv.sh
@@ -5,9 +5,28 @@ set -euxo pipefail
 #    quickly see the wireguard configuration.
 apt-get install -y wireguard
 
-helm repo add jenkins https://charts.jenkins.io
+#DEPLOY JENKINS ON K
+# .........................8S
+# helm repo add jenkins https://charts.jenkins.io
+# helm repo update
+# helm upgrade --install jenkins-4.3.23 jenkins/jenkins --kubeconfig /etc/rancher/k3s/k3s.yaml
+
+helm repo add jenkinsci https://charts.jenkins.io
 helm repo update
-helm upgrade --install jenkins-4.3.23 jenkins/jenkins --kubeconfig /etc/rancher/k3s/k3s.yaml
+helm search repo jenkinsci
+kubectl apply -f ../k8s-manifests/jenkins-volume.yaml
+sudo chown -R 1000:1000 /data/jenkins-volume
+kubectl apply -f ../k8s-manifests/jenkins-sa.yaml
+chart=jenkinsci/jenkins
+helm install jenkins -n jenkins -f ../k8s-manifests/jenkins-values.yaml $chart
+jsonpath="{.data.jenkins-admin-password}"
+secret=$(kubectl get secret -n jenkins jenkins -o jsonpath=$jsonpath)
+echo $(echo $secret | base64 --decode)
+jsonpath="{.spec.ports[0].nodePort}"
+NODE_PORT=$(kubectl get -n jenkins -o jsonpath=$jsonpath services jenkins)
+jsonpath="{.items[0].status.addresses[0].address}"
+NODE_IP=$(kubectl get nodes -n jenkins -o jsonpath=$jsonpath)
+echo http://$NODE_IP:$NODE_PORT/login
 
 
 $ kubectl exec --namespace default -it svc/myjenkins -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo
